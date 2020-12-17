195be8186b6a36b070925f60052a830fc0bb1d5f
Janos Hausberger <hausberger.janos@codeyard.eu>
2020-12-17 10:55:05 +0100

    SampleFragment has been added

---
+++ app/src/main/java/com/hausberger/mvistarter/framework/presentation/sample/SampleFragment.kt
@@ -0,0 +1,9 @@
+package com.hausberger.mvistarter.framework.presentation.sample
+
+import androidx.fragment.app.Fragment
+import com.hausberger.mvistarter.R
+
+class SampleFragment : Fragment(R.layout.fragment_sample) {
+
+
+}
