06a56b59bc24f19bf49774f5244eac72196a1220
Janos Hausberger <hausberger.janos@codeyard.eu>
2020-12-28 14:49:17 +0100

    BottomNavBar has been removed

--- app/src/main/java/com/hausberger/mvistarter/framework/presentation/MainActivity.kt
+++ app/src/main/java/com/hausberger/mvistarter/framework/presentation/MainActivity.kt
@@ -1,84 +1,14 @@
 package com.hausberger.mvistarter.framework.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
 import com.hausberger.mvistarter.R
-import com.hausberger.mvistarter.framework.presentation.common.BottomNavController
-import com.hausberger.mvistarter.framework.presentation.common.BottomNavController.*
-import com.hausberger.mvistarter.framework.presentation.common.setUpNavigation
-import com.hausberger.mvistarter.framework.presentation.details.DetailsFragment
-import com.hausberger.mvistarter.util.Constants.BundleKeys.Companion.BOTTOM_NAV_BACK_STACK_KEY
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
-class MainActivity : BaseActivity(), OnNavigationGraphChanged, OnNavigationReselectedListener {
-
-    private val bottomNavController by lazy(LazyThreadSafetyMode.NONE) {
-        BottomNavController(
-            this,
-            R.id.fragmentContainer,
-            R.id.menu_a,
-            this
-        )
-    }
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        setupBottomNavigation(savedInstanceState)
-    }
-
-    private fun setupBottomNavigation(savedInstanceState: Bundle? = null) {
-        bottomNavigationView?.setUpNavigation(
-            bottomNavController = bottomNavController,
-            onReselectListener = this
-        )
-
-        if (savedInstanceState == null) {
-            bottomNavController.setupBottomNavigationBackStack(null)
-            bottomNavController.onNavigationItemSelected()
-        } else {
-            (savedInstanceState[BOTTOM_NAV_BACK_STACK_KEY] as IntArray?)?.let { items ->
-                val backStack = BackStack()
-                backStack.addAll(items.toTypedArray())
-                bottomNavController.setupBottomNavigationBackStack(backStack)
-            }
-        }
-    }
-
-    override fun onBackPressed() {
-        if (bottomNavController.isNavigationBackStackInitialized()) {
-            bottomNavController.onBackPressed()
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-
-        // save backstack for bottom nav
-        if (bottomNavController.isNavigationBackStackInitialized()) {
-            outState.putIntArray(
-                BOTTOM_NAV_BACK_STACK_KEY,
-                bottomNavController.navigationBackStack.toIntArray()
-            )
-        }
-    }
-
-    override fun onGraphChange() {
-        // in this sample we have to nothing to do here
-    }
-
-    override fun onReselectNavItem(navController: NavController, fragment: Fragment) {
-        when (fragment) {
-
-            is DetailsFragment -> {
-                navController.navigate(R.id.action_detailsFragment_to_home)
-            }
-        }
     }
 }
